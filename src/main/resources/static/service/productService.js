bd0d5dbf16a0593b0a29324658f2068c8b08784b
smatine <soufiane.matine@gmail.com>
2017-11-17 13:43:16 +0100

    Update productService.js

--- src/main/resources/static/service/productService.js
+++ src/main/resources/static/service/productService.js
@@ -25,7 +25,7 @@ microservice
 					this.getdata = function(callbackData) {
 
 						var responsePromise = $http
-								.get("https://ms-api-gateway-toto.apps.oc.smatine.com/isadco/tempAppApiGateway/api/product/products/all");
+								.get("http://ms-api-gateway-toto.apps.oc.smatine.com/isadco/tempAppApiGateway/api/product/products/all");
 						responsePromise.success(function(data, status, headers,
 								config) {
 							callbackData(data);
